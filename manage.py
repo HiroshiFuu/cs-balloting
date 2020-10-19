1cd08f4fcb4a5feb6356c73f4312cf01a03e6864
Feng Hao <hiroshifuu@outlook.com>
2020-10-20 00:21:37 +0800

    feat: load setting in manage.py using .env

--- manage.py
+++ manage.py
@@ -6,9 +6,16 @@ Copyright (c) 2019 - present AppSeed.us
 
 import os
 import sys
+import environ
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.local')
+    env = environ.Env(
+        # set casting, default value
+        DEBUG=(bool, False)
+    )
+    environ.Env.read_env(env_file=os.path.join(os.getcwd(), '.env'))
+    RUN_ENV = env.str('RUN_ENV', 'local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.' + RUN_ENV)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
