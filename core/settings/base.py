753d9f7eb1cab547ff497fa7c685300ff33460fc
Feng Hao <hiroshifuu@outlook.com>
2020-11-02 04:56:49 +0800

    fix: renaming

--- core/settings/base.py
+++ core/settings/base.py
@@ -200,7 +200,7 @@ SUIT_CONFIG = {
     'MENU': (
         {'app': 'authentication', 'label': 'Authentication and Authentication', 'icon':'icon-lock', 'models': ('authuser', 'authgroup', 'company')},
         {'app': 'ballot'},
-        {'app': 'live_poll_multiple'},
+        {'app': 'live_poll_multiple', 'label': 'Live Poll Multiple'},
         'sites',
     ),
 
