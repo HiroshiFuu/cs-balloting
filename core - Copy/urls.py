06089f58752d0b3a8d33fa223217e3c82b3e3d16
Feng Hao <hiroshifuu@outlook.com>
2020-11-23 22:25:00 +0800

    feat: backup core

---
+++ core - Copy/urls.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('accounts/', include('allauth.urls')),
+    path('', include('authentication.urls')),
+    path('', include('ballot.urls')),
+    path('', include('live_poll_multiple.urls')),
+]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
