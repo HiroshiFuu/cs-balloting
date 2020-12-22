1ee048f21b4536a42a3ce6f3582b3197084c3ef2
Feng Hao <hiroshifuu@outlook.com>
2020-12-23 00:32:01 +0800

    fix: stupid 'YES'

--- core/settings/deployment-ssl.py
+++ core/settings/deployment-ssl.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['118.201.195.130', 'eagm.cstechsolutions.com.sg']
 
 RUNSERVERPLUS_SERVER_ADDRESS_PORT = '0.0.0.0:443'
 
-SECURE_SSL_REDIRECT = YES
+SECURE_SSL_REDIRECT = True
 
 DATABASES = {
     'default': {
