14ab7d7096a1dafca06fa6141df93dc74b92b798
Feng Hao <hiroshifuu@outlook.com>
2021-01-18 19:20:55 +0800

    fix: temporally enable cors for ssl

--- core/settings/deployment-ssl.py
+++ core/settings/deployment-ssl.py
@@ -7,8 +7,18 @@ from .deployment import *  # noqa: F401
 
 SECRET_KEY = 'jyejjj47s!t1zs%nd@8g4(+qmk(si66ekm1ki!_dhgoe_--iip'
 
-DEBUG = True
+DEBUG = False
 
 RUNSERVERPLUS_SERVER_ADDRESS_PORT = '0.0.0.0:443'
 
 SECURE_SSL_REDIRECT = True
+
+ENVIRON_APPS = [
+    'corsheaders',
+]
+
+INSTALLED_APPS += ENVIRON_APPS
+
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+
+CORS_ALLOW_ALL_ORIGINS = True
